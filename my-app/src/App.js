95d515e7efd2dde7c9442e8d0745ccfdac6c2ef6
Paul Bowden <paulbowden673@gmail.com>
2022-02-23 12:48:24 +0000

    challenge 10

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,8 +3,9 @@ import css from './App.module.css';
 import Sidebar from "./components/Sidebar";
 import NavBarForm from "./components/NavBarForm";
 import Content from "./components/Content";
-
-
+import ContentHooks from "./components/ContentHooks";
+import ContentAPI from "./components/ContentAPI";
+import ContentAPIHooks from "./components/ContentAPIHooks";
 
 function App() {
   return (
@@ -13,7 +14,10 @@ function App() {
       <Sidebar />
      
       <NavBarForm />
-      <Content />
+      {/*<Content />*/}
+      {/*<ContentHooks />*/}
+      {/*<ContentAPI />*/}
+      <ContentAPIHooks />
     </div>
   );
 }
